6c82d0d13b002432688dcaaec9c8e8a9253f82d2
RinZ <supawit.promma6256@gmail.com>
2026-01-13 23:55:06 +0700

    feat(api): setup Gin server and add /api/quote endpoint

---
+++ backend/cmd/server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/rinz5/stock-tracker/backend/internal/finnhub"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error: .evn file is not found or could not be loaded.")
+	}
+
+	apiKey := os.Getenv("FINNHUB_API_KEY")
+	if apiKey == "" {
+		log.Fatal("Error: FINNHUB_API_KEY is not set.")
+	}
+
+	client := finnhub.NewClient(apiKey)
+
+	r := gin.Default()
+
+	r.GET("/api/quote", func(ctx *gin.Context) {
+		symbol := ctx.Query("symbol")
+		if symbol == "" {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Symbol is required"})
+			return
+		}
+
+		quote, err := client.GetQuote(symbol)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, quote)
+	})
+
+	log.Println("Server running on http://localhost:8080")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
+}
