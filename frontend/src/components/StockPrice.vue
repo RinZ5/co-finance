357f16f00638fd30af3ceaf9b55f087073189122
RinZ <supawit.promma6256@gmail.com>
2026-01-30 22:59:37 +0700

    fix(frontend): remove unused import

--- frontend/src/components/StockPrice.vue
+++ frontend/src/components/StockPrice.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted } from 'vue';
+import { computed } from 'vue';
 import type { StockQuote } from '../types/types.ts'
 
 const props = defineProps<{
